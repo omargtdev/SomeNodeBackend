315e73c3a6d293e6a80df699b93f4a130a3bdbdd
omargtdev <omar.gutierrez.tafur@gmail.com>
2022-11-15 19:11:58 -0500

    CORS added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express from 'express';
 import { join } from 'path';
+
+import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 // App
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // Receive also form data
 
